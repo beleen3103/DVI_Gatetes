e5a913853e12739240af6fd006bf0a8b2ad6b3a4
Miguel Gutierrez Vazquez <miggut02@ucm.es>
2022-02-15 15:40:44 +0100

    Health Bar
    Barra de vida para todos los personajes.
    Crea la barra en la posición (x,y) y calcula el porcentaje de vida perdida cada vez que pierde vida.

---
+++ PruebaUwU/src/healthBar.js
@@ -0,0 +1,29 @@
+export default class HealthBar{
+    constructor(scene, x, y,maxHealth){
+        this.bar = new Phaser.GameObjects.Graphics(scene);
+        this.x=x;
+        this.y=y;
+        this.maxHealth = maxHealth;
+        this.health = maxHealth;
+        this.draw();
+        scene.add.existing(this.bar);
+    }
+    decrease(x){
+        if(this.health > 0) this.health -= x;
+        else this.health = 0;
+        this.draw();
+    }
+
+    isDead(){
+        return this.health === 0;
+    }
+
+    draw(){
+        this.bar.clear();
+        this.bar.fillStyle(0x000000);
+        this.bar.fillRect(this.x,this.y,100,16);
+        this.bar.fillStyle(0x008000);
+        var d = Math.floor(this.health/this.maxHealth*100);
+        if(!this.isDead()) this.bar.fillRect(this.x,this.y,d,16);
+    }
+}
