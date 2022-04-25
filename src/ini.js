20270b19743f6ccfa62345dd9298b4f6f6c875b9
MiquelVera <mivera@ucm.es>
2022-04-25 20:26:28 +0200

    Pantalla Inicio

--- src/ini.js
+++ src/ini.js
@@ -33,6 +33,6 @@ export default class Ini extends Phaser.Scene {
     }
 
     create() {
-        this.scene.start('tutorial', {x:100,y:490, numeroAnimales: 2, animal1: 'Gato', animal1Vida: 100, animal2: '.', animal2Vida: 0, animal3: '.', animal3Vida: 0, actual: 'Gato', flip: true});
+        this.scene.start('pantallaInicio', {x:100,y:490, numeroAnimales: 1, animal1: 'Gato', animal1Vida: 100, animal2: '.', animal2Vida: 0, animal3: '.', animal3Vida: 0, actual: 'Gato', flip: true});
     }
 }
