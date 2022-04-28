2a09e4f25fc1797089804e75b54e703f4d190e8c
Miguel Gutierrez Vazquez <miggut02@ucm.es>
2022-04-28 15:22:11 +0200

    Add files via upload

---
+++ assets/sprites/alcantarillas.tsx
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="alcantarillas" tilewidth="30" tileheight="30" tilecount="14" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="colisiona" type="bool" value="true"/>
+  </properties>
+  <image width="30" height="30" source="alcantarillaS1.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="colisiona" type="bool" value="true"/>
+  </properties>
+  <image width="30" height="30" source="alcantarillaS2.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="colisiona" type="bool" value="true"/>
+  </properties>
+  <image width="30" height="30" source="alcantarillaS3.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="colisiona" type="bool" value="true"/>
+  </properties>
+  <image width="30" height="30" source="alcantarillaS4.png"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="colisiona" type="bool" value="true"/>
+  </properties>
+  <image width="30" height="30" source="alcantarillaS5.png"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="colisiona" type="bool" value="true"/>
+  </properties>
+  <image width="30" height="30" source="madera.png"/>
+ </tile>
+ <tile id="6" type="bajoSuelo1">
+  <properties>
+   <property name="colisiona" type="bool" value="true"/>
+  </properties>
+  <image width="30" height="30" source="op1SubSuelo.png"/>
+ </tile>
+ <tile id="7" type="Suelo agua 1">
+  <image width="30" height="30" source="op1Suelo.png"/>
+ </tile>
+ <tile id="8" type="Suelo2">
+  <properties>
+   <property name="colisiona" type="bool" value="true"/>
+  </properties>
+  <image width="30" height="30" source="op2SubSuelo.png"/>
+ </tile>
+ <tile id="9" type="Suelo charquito1">
+  <image width="30" height="30" source="op2Suelo.png"/>
+ </tile>
+ <tile id="10" type="Suelo1">
+  <image width="30" height="30" source="op3Suelo.png"/>
+ </tile>
+ <tile id="11" type="Suelo2.1">
+  <properties>
+   <property name="colisiona" type="bool" value="true"/>
+  </properties>
+  <image width="30" height="30" source="op4Suelo.png"/>
+ </tile>
+ <tile id="12" type="Suelo2.2">
+  <properties>
+   <property name="colisiona" type="bool" value="true"/>
+  </properties>
+  <image width="30" height="30" source="op5Suelo.png"/>
+ </tile>
+ <tile id="14">
+  <image width="30" height="30" source="tile2.png"/>
+ </tile>
+</tileset>
