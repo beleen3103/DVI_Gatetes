2625b270e8fa2c2f9c4cb3d88e8d0645cfd61bfc
javife15 <60519230+javife15@users.noreply.github.com>
2022-05-06 20:13:05 +0200

    done

--- src/ini.js
+++ src/ini.js
@@ -6,11 +6,6 @@ export default class Ini extends Phaser.Scene {
 
     preload() {
         this.load.setPath('assets/sprites/');
-        this.load.image('platformBasica', 'platform.png');
-        this.load.image('platformAtravesable', 'platformT.png');
-        this.load.image('platformEnredadera','platformE.png');
-        this.load.image('platformRebote','platformR.png');
-        this.load.spritesheet('platformRompe', 'platformA.png', { frameWidth: 256, frameHeight: 64 });
         this.load.image('emo', 'emo.png');
         this.load.image('nota', 'nota.png');
         this.load.image('rockero', 'rockero.png');
@@ -24,8 +19,6 @@ export default class Ini extends Phaser.Scene {
         this.load.spritesheet('Gato', 'gato.png', { frameWidth: 150, frameHeight: 150 });
         this.load.spritesheet('Rata', 'rata.png', { frameWidth: 150, frameHeight: 150 });
 
-        this.load.image('puertaCombate', 'puertaCombate.png');
-        this.load.image('puertaPlataformeo', 'puertaPlataformeo.png');
         this.load.image('flecha', 'flecha.png');
         this.load.image('afilar', 'afilar.png');
         this.load.image('zarpazo','arañazo.png');
